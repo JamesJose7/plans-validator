7bb3d5025684b84aa39a8a38328c4bf49c1468b1
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2020-01-23 11:00:38 -0500

    Initial commit

---
+++ src/main/java/com/utpl/plansvalidator/PlansValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.utpl.plansvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlansValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlansValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utpl/plansvalidator/PlansValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utpl.plansvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlansValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
